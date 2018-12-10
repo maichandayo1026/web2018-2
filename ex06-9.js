e25397c12cc7b63e0a396f74303c23db06a9ad24
moeka2102 <moeka2102@gmail.com>
2018-12-09 20:56:40 -0500

    message

--- ex06-9.js
+++ ex06-9.js
@@ -1,45 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<title>タイトル</title>
-<link rel="stylesheet"href="mystyle.css">
-<script src="ex06-5.js"></script>
-<script src="ex06-6.js"></script>
-<script src="ex06-7.js"></script>
-<script src="ex06-8.js"></script>
-
-</head>
-  
-<body>
-<h1>変数の練習65</h1>
-<input type="button"value="あいさつ"onclick="sayhello65();">
-<input type="button"value="太郎"onclick="taro65();">
-<input type="button"value="花子"onclick="hanako65();">
-  
-<h1>変数の練習66</h1>
-<input type="button"value="あいさつ"onclick="sayhello66();">
-<input type="button"value="太郎"onclick="taro66();">
-<input type="button"value="花子"onclick="hanako66();">
-  
-  <h1>変数の練習67</h1>
-  <input type="button"value="あいさつ"onclick="sayhello67();">
-  <input type="button"value="太郎"onclick="someone67('太郎');">
-  <input type="button"value="花子"onclick="someone67('花子');">
-  <input type="button"value="レイ"onclick="someone67('レイ');">
-  <input type="button"value="アスカ"onclick="someone67('アスカ');">
-  <input type="button"value="シンジ"onclick="someone67('シンジ');">
-
-  <h1>JavaScriptによる変更の練習68</h1>
- <input type="button" value="あいさつ" onclick="sayhello68();">
- <input type="button" value="太郎" onclick="taro68();">
- <input type="button" value="花子" onclick="hanako68();">
-  <p id="who68">最初はworld</p>
-  
-  <h1>JavaScriptによるページの変更69</h1>
-  <div id="txt1">
-    <input type="button" value="テキストの変更"onclick="txtchange1();">
-    城西大学</div>
-  
-  </body>
-</html>
+function imgchange1(){
+  document.getElementById('logo').scr =
+    './image/bulbon.gif';
+}
